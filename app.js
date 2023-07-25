10610ea84822ffa970a6e1e9a7c289ed6dce7610
Cris-aian Vergara <cris.vergara@achievewithoutborders.com>
2023-07-25 11:30:38 +0800

    First commit.

---
+++ app.js
@@ -0,0 +1,48 @@
+(function () {
+    console.log("hello owl", owl.__info__.version);
+
+    // -------------------------------------------------------------------------
+    // Task Component
+    // -------------------------------------------------------------------------
+    class Task extends Component {
+        static template = xml /* xml */`
+        <div class="task" t-att-class="props.task.isCompleted ? 'done' : ''">
+            <input type="checkbox" t-att-checked="props.task.isCompleted"/>
+            <span><t t-esc="props.task.text"/></span>
+        </div>`;
+        
+        static props = ["task"];
+    }
+    
+    // -------------------------------------------------------------------------
+    // Root Component
+    // -------------------------------------------------------------------------
+    class Root extends Component {
+        static template = xml /* xml */`
+        <div class="task-list">
+            <t t-foreach="tasks" t-as="task" t-key="task.id">
+                <Task task="task"/>
+            </t>
+        </div>`;
+        static components = { Task };
+    
+        tasks = [
+            {
+                id: 1,
+                text: "buy milk",
+                isCompleted: true,
+            },
+            {
+                id: 2,
+                text: "clean house",
+                isCompleted: false,
+            },
+        ];
+    }
+    
+    // -------------------------------------------------------------------------
+    // Setup
+    // -------------------------------------------------------------------------
+    mount(Root, document.body, {dev: true});
+    
+  })();
