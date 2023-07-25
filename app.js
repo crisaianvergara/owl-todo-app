b219fcbb7ca6fb63ca3c77d44a3ea0bda718da56
Cris-aian Vergara <cris.vergara@achievewithoutborders.com>
2023-07-25 13:49:22 +0800

    Starter code.

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
 (function () {
     console.log("hello owl", owl.__info__.version);
 
+    const { Component, mount, xml, useRef, onMounted, useState } = owl;
+
     // -------------------------------------------------------------------------
     // Task Component
     // -------------------------------------------------------------------------
     class Task extends Component {
         static template = xml /* xml */`
         <div class="task" t-att-class="props.task.isCompleted ? 'done' : ''">
-            <input type="checkbox" t-att-checked="props.task.isCompleted"/>
+            <input type="checkbox" t-att-checked="props.task.isCompleted" t-on-click="toggleTask"/>
             <span><t t-esc="props.task.text"/></span>
+            <span class="delete" t-on-click="deleteTask">🗑</span>
         </div>`;
-        
-        static props = ["task"];
+
+        static props = ["task", "onDelete"];
+
+        toggleTask() {
+            this.props.task.isCompleted = !this.props.task.isCompleted;
+        }
+
+        deleteTask() {
+            this.props.onDelete(this.props.task);
+        }
     }
     
     // -------------------------------------------------------------------------
@@ -19,25 +30,62 @@
     // -------------------------------------------------------------------------
     class Root extends Component {
         static template = xml /* xml */`
-        <div class="task-list">
-            <t t-foreach="tasks" t-as="task" t-key="task.id">
-                <Task task="task"/>
-            </t>
+        <div class="todo-app">
+            <input placeholder="Enter a new task" t-on-keyup="addTask" t-ref="add-input" />
+            <div class="task-list">
+                <t t-foreach="tasks" t-as="task" t-key="task.id">
+                    <Task task="task" onDelete.bind="deleteTask"/>
+                </t>
+            </div>
         </div>`;
+        
+        
         static components = { Task };
+
+        // in App
+        setup() {
+            const inputRef = useRef("add-input");
+            onMounted(() => inputRef.el.focus());
+        }    
     
-        tasks = [
-            {
-                id: 1,
-                text: "buy milk",
-                isCompleted: true,
-            },
-            {
-                id: 2,
-                text: "clean house",
-                isCompleted: false,
-            },
-        ];
+        // tasks = [
+        //     {
+        //         id: 1,
+        //         text: "buy milk",
+        //         isCompleted: true,
+        //     },
+        //     {
+        //         id: 2,
+        //         text: "clean house",
+        //         isCompleted: false,
+        //     },
+        // ];
+
+        nextId = 1;
+        tasks = useState([]);
+
+        addTask(ev) {
+            // 13 is keycode for ENTER
+            if (ev.keyCode === 13) {
+                const text = ev.target.value.trim();
+                ev.target.value = "";
+                if (text) {
+                    const newTask = {
+                        id: this.nextId++,
+                        text: text,
+                        isCompleted: false,
+                    };
+                    this.tasks.push(newTask);
+                    console.log(this.tasks)
+                }
+            }
+        }
+
+        deleteTask(task) {
+            const index = this.tasks.findIndex(t => t.id === task.id);
+            this.tasks.splice(index, 1);
+        }
+
     }
     
     // -------------------------------------------------------------------------
