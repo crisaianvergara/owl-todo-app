1c9cfe47f9e3f8ebd5517b2b56bc4e18ad28e08e
Cris-aian Vergara <cris.vergara@achievewithoutborders.com>
2023-07-25 14:17:20 +0800

    Done store.

--- app.js
+++ app.js
@@ -1,96 +1,113 @@
 (function () {
     console.log("hello owl", owl.__info__.version);
 
-    const { Component, mount, xml, useRef, onMounted, useState } = owl;
+    const { Component, mount, xml, useRef, onMounted, useState, reactive, useEnv } = owl;
+
+    // -------------------------------------------------------------------------
+    // Store
+    // -------------------------------------------------------------------------
+    function useStore() {
+        const env = useEnv();
+        return useState(env.store);
+    }
+
+    // -------------------------------------------------------------------------
+    // TaskList
+    // -------------------------------------------------------------------------
+    class TaskList {
+        constructor(tasks) {
+            this.tasks = tasks || [];
+            const taskIds = this.tasks.map((t) => t.id);
+            this.nextId = taskIds.length ? Math.max(...taskIds) + 1 : 1;
+        }
+
+        nextId = 1;
+        tasks = [];
+
+        addTask(text) {
+            text = text.trim();
+            if (text) {
+            const task = {
+                id: this.nextId++,
+                text: text,
+                isCompleted: false,
+            };
+            this.tasks.push(task);
+            }
+        }
+
+        toggleTask(task) {
+            task.isCompleted = !task.isCompleted;
+        }
+
+        deleteTask(task) {
+            const index = this.tasks.findIndex((t) => t.id === task.id);
+            this.tasks.splice(index, 1);
+        }
+    }
+
+    function createTaskStore() {
+        const saveTasks = () => localStorage.setItem("todoapp", JSON.stringify(taskStore.tasks));
+        const initialTasks = JSON.parse(localStorage.getItem("todoapp") || "[]");
+        const taskStore = reactive(new TaskList(initialTasks), saveTasks);
+        saveTasks();
+        return taskStore;
+    }
 
     // -------------------------------------------------------------------------
     // Task Component
     // -------------------------------------------------------------------------
     class Task extends Component {
-        static template = xml /* xml */`
-        <div class="task" t-att-class="props.task.isCompleted ? 'done' : ''">
-            <input type="checkbox" t-att-checked="props.task.isCompleted" t-on-click="toggleTask"/>
+        static template = xml/* xml */ `
+            <div class="task" t-att-class="props.task.isCompleted ? 'done' : ''">
+            <input type="checkbox" t-att-checked="props.task.isCompleted" t-on-click="() => store.toggleTask(props.task)"/>
             <span><t t-esc="props.task.text"/></span>
-            <span class="delete" t-on-click="deleteTask">🗑</span>
-        </div>`;
+            <span class="delete" t-on-click="() => store.deleteTask(props.task)">🗑</span>
+            </div>`;
 
-        static props = ["task", "onDelete"];
+        static props = ["task"];
 
-        toggleTask() {
-            this.props.task.isCompleted = !this.props.task.isCompleted;
-        }
-
-        deleteTask() {
-            this.props.onDelete(this.props.task);
+        setup() {
+            this.store = useStore();
         }
     }
-    
+
     // -------------------------------------------------------------------------
     // Root Component
     // -------------------------------------------------------------------------
     class Root extends Component {
-        static template = xml /* xml */`
-        <div class="todo-app">
-            <input placeholder="Enter a new task" t-on-keyup="addTask" t-ref="add-input" />
+        static template = xml/* xml */ `
+            <div class="todo-app">
+            <input placeholder="Enter a new task" t-on-keyup="addTask" t-ref="add-input"/>
             <div class="task-list">
-                <t t-foreach="tasks" t-as="task" t-key="task.id">
-                    <Task task="task" onDelete.bind="deleteTask"/>
+                <t t-foreach="store.tasks" t-as="task" t-key="task.id">
+                <Task task="task"/>
                 </t>
             </div>
-        </div>`;
-        
-        
+            </div>`;
         static components = { Task };
 
-        // in App
         setup() {
             const inputRef = useRef("add-input");
             onMounted(() => inputRef.el.focus());
-        }    
-    
-        // tasks = [
-        //     {
-        //         id: 1,
-        //         text: "buy milk",
-        //         isCompleted: true,
-        //     },
-        //     {
-        //         id: 2,
-        //         text: "clean house",
-        //         isCompleted: false,
-        //     },
-        // ];
-
-        nextId = 1;
-        tasks = useState([]);
+            this.store = useStore();
+        }
 
         addTask(ev) {
             // 13 is keycode for ENTER
             if (ev.keyCode === 13) {
-                const text = ev.target.value.trim();
-                ev.target.value = "";
-                if (text) {
-                    const newTask = {
-                        id: this.nextId++,
-                        text: text,
-                        isCompleted: false,
-                    };
-                    this.tasks.push(newTask);
-                    console.log(this.tasks)
-                }
+            this.store.addTask(ev.target.value);
+            ev.target.value = "";
             }
         }
-
-        deleteTask(task) {
-            const index = this.tasks.findIndex(t => t.id === task.id);
-            this.tasks.splice(index, 1);
-        }
-
     }
-    
+
     // -------------------------------------------------------------------------
     // Setup
     // -------------------------------------------------------------------------
-    mount(Root, document.body, {dev: true});
+    const env = {
+        store: createTaskStore(),
+    };
+    mount(Root, document.body, { dev: true, env });
     
   })();
